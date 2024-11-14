ce02e1eeb0c306a3e57c03aeacca6d10e8198991
RemasNg1 <remas-2290@hotmail.com>
2024-11-14 11:48:45 +0300

    Add project files.

---
+++ project/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using System;
+using System.Xml.Linq;
+
+
+namespace Cproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>();
+            List<double> Degrees = new List<double>();
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter the student's name (or type '#' to finish):");
+                string StuName = Console.ReadLine();
+                if (StuName=="#")
+                {
+                    break;
+                }
+                Console.WriteLine($"Enter the grade for {StuName}: ");
+                double StuDeg = double.Parse(Console.ReadLine());
+
+
+                Names.Add(StuName);
+                Degrees.Add(StuDeg);
+               
+                //Console.WriteLine(Names+" "+Degrees);
+
+            }
+            Console.WriteLine("{0,-20} {1,8} {2,10}","Student Name","Mark", "Status");
+            Console.WriteLine("----------------------------------------------");
+            for (int i=0; i<Names.Count; i++)
+            {
+                string Status = Degrees[i]>=60 ? "Passed" : "Failed";
+                Console.WriteLine("{0,-20}{1,8}{2,12}", Names[i], Degrees[i],Status);
+               
+                }
+            }
+        }
+    }
